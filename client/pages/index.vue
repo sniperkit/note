91a58c13ffa02a71bbb7bd921d884e757266cef6
wxaxiaoyao <765485868@qq.com>
2018-05-21 19:26:35 +0800

    xxx

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-	<div>welconme to note</div>
+	<div>
+		welconme to note
+		<div style="color:blue">
+			<ul>
+				<li>list1</li>
+				<li>list2</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script>
+import {
+	DatePicker,
+} from "element-ui";
+
+import media from "@/components/bases/media.vue";
+
 export default {
 	layout: "index",
+
+	components: {
+		media,
+	},
 }
 </script>
