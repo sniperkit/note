a1603460ce2db0f64d19ddb5ccd6d7d983b1f740
xiaoyao <765485868@qq.com>
2018-05-30 22:22:48 +0800

    xx

--- server/controllers/code.js
+++ server/controllers/code.js
@@ -9,7 +9,7 @@ export const Code = function() {
 
 Code.prototype.pushCode = function() {
 	var cmd_str = "cd " + this.rootdir + "; git reset --hard HEAD; git pull origin master;";
-	cmd_str += "wget 'http://qiniu.wxaxiaoyao.cn/note/note/mods.md?attname=&e=1525602488&token=qPCxQ1ZsJnhESNNftGWh5oTaDesVgzM_O4O0h8Wg:axONBqLmRwx3Fh9NN84BwzHvG2E' -O ./client/components/mods/mods.yml";
+	//cmd_str += "wget 'http://qiniu.wxaxiaoyao.cn/note/note/mods.md?attname=&e=1525602488&token=qPCxQ1ZsJnhESNNftGWh5oTaDesVgzM_O4O0h8Wg:axONBqLmRwx3Fh9NN84BwzHvG2E' -O ./client/components/mods/mods.yml";
 	shell.exec(cmd_str);
 }
 
