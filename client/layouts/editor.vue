cfc7c519e782f37e2da53e554492fbf73e2af988
wxaxiaoyao <765485868@qq.com>
2018-06-13 17:50:25 +0800

    xxx

---
+++ client/layouts/editor.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="layoutContainer">
+	</div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped>
+.layoutContainer {
+	height: 100%;
+}
+</style>
